969f62c87e95ee36abef6877e4b6c5faec3cea1b
Vit <vit_home@mail.com>
2013-10-17 02:22:52 +0300

    - bugfix at TupleFromList

--- Data/Tuple/PolyParts/List.hs
+++ Data/Tuple/PolyParts/List.hs
@@ -63,71 +63,86 @@ type MonoTuple15 a = (a, a, a, a, a, a, a, a, a, a, a, a, a, a, a)
 --   @
 --
 class TupleFromList a where
-    tupleFromList :: [a] -> a
+    type PartTupleList a    
+    tupleFromList :: [PartTupleList a] -> a
 
     
-instance TupleToList () where
-    tupleFromList _ = []
     
-instance TupleToList (MonoTuple1 a) where
+instance (PartTupleList a ~ a) => TupleFromList (MonoTuple1 a) where
+    type PartTupleList (MonoTuple1 a) = a    
     tupleFromList (a:_) = OneTuple a
     tupleFromList _     = error "TupleToList: not enoght elements in list for MonoTuple1"
     
-instance TupleToList (MonoTuple2 a) where
+instance (PartTupleList a ~ a) => TupleFromList (MonoTuple2 a) where
+    type PartTupleList (MonoTuple2 a) = a    
     tupleFromList (a:b:_) = (a, b)
     tupleFromList _     = error "TupleToList: not enoght elements in list for MonoTuple2"
 
-instance TupleToList (MonoTuple3 a) where
+instance (PartTupleList a ~ a) => TupleFromList (MonoTuple3 a) where
+    type PartTupleList (MonoTuple3 a) = a    
     tupleFromList (a1:a2:a3:_) = (a1, a2, a3)
     tupleFromList _     = error "TupleToList: not enoght elements in list for MonoTuple3"
 
-instance TupleToList (MonoTuple4 a) where
-    tupleFromList (a1:a2:a3::a4_) = (a1, a2, a3, a4)
+instance (PartTupleList a ~ a) => TupleFromList (MonoTuple4 a) where
+    type PartTupleList (MonoTuple4 a) = a    
+    tupleFromList (a1:a2:a3:a4:_) = (a1, a2, a3, a4)
     tupleFromList _     = error "TupleToList: not enoght elements in list for MonoTuple4"
 
-instance TupleToList (MonoTuple5 a) where
-    tupleFromList (a1:a2:a3::a4:a5:_) = (a1, a2, a3, a4, a5)
+instance (PartTupleList a ~ a) => TupleFromList (MonoTuple5 a) where
+    type PartTupleList (MonoTuple5 a) = a    
+    tupleFromList (a1:a2:a3:a4:a5:_) = (a1, a2, a3, a4, a5)
     tupleFromList _     = error "TupleToList: not enoght elements in list for MonoTuple5"
 
-instance TupleToList (MonoTuple6 a) where
-    tupleFromList (a1:a2:a3::a4:a5:a6:_) = (a1, a2, a3, a4, a5, a6)
+instance (PartTupleList a ~ a) => TupleFromList (MonoTuple6 a) where
+    type PartTupleList (MonoTuple6 a) = a    
+    tupleFromList (a1:a2:a3:a4:a5:a6:_) = (a1, a2, a3, a4, a5, a6)
     tupleFromList _     = error "TupleToList: not enoght elements in list for MonoTuple6"
 
-instance TupleToList (MonoTuple7 a) where
-    tupleFromList (a1:a2:a3::a4:a5:a6:a7:_) = (a1, a2, a3, a4, a5, a6, a7)
+instance (PartTupleList a ~ a) => TupleFromList (MonoTuple7 a) where
+    type PartTupleList (MonoTuple7 a) = a    
+    tupleFromList (a1:a2:a3:a4:a5:a6:a7:_) = (a1, a2, a3, a4, a5, a6, a7)
     tupleFromList _     = error "TupleToList: not enoght elements in list for MonoTuple7"
 
-instance TupleToList (MonoTuple8 a) where
-    tupleFromList (a1:a2:a3::a4:a5:a6:a7:a8:_) = (a1, a2, a3, a4, a5, a6, a7, a8)
+instance (PartTupleList a ~ a) => TupleFromList (MonoTuple8 a) where
+    type PartTupleList (MonoTuple8 a) = a    
+    tupleFromList (a1:a2:a3:a4:a5:a6:a7:a8:_) = (a1, a2, a3, a4, a5, a6, a7, a8)
     tupleFromList _     = error "TupleToList: not enoght elements in list for MonoTuple8"
 
-instance TupleToList (MonoTuple9 a) where
-    tupleFromList (a1:a2:a3::a4:a5:a6:a7:a8:a9:_) = (a1, a2, a3, a4, a5, a6, a7, a8, a9)
+instance (PartTupleList a ~ a) => TupleFromList (MonoTuple9 a) where
+    type PartTupleList (MonoTuple9 a) = a    
+    tupleFromList (a1:a2:a3:a4:a5:a6:a7:a8:a9:_) = (a1, a2, a3, a4, a5, a6, a7, a8, a9)
     tupleFromList _     = error "TupleToList: not enoght elements in list for MonoTuple9"
 
-instance TupleToList (MonoTuple10 a) where
-    tupleFromList (a1:a2:a3::a4:a5:a6:a7:a8:a9:a10:_) = (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10)
+instance (PartTupleList a ~ a) => TupleFromList (MonoTuple10 a) where
+    type PartTupleList (MonoTuple10 a) = a    
+    tupleFromList (a1:a2:a3:a4:a5:a6:a7:a8:a9:a10:_) = (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10)
     tupleFromList _     = error "TupleToList: not enoght elements in list for MonoTuple10"
 
-instance TupleToList (MonoTuple11 a) where
-    tupleFromList (a1:a2:a3::a4:a5:a6:a7:a8:a9:a10:a11:_) = (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11)
+instance (PartTupleList a ~ a) => TupleFromList (MonoTuple11 a) where
+    type PartTupleList (MonoTuple11 a) = a    
+    tupleFromList (a1:a2:a3:a4:a5:a6:a7:a8:a9:a10:a11:_) = (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11)
     tupleFromList _     = error "TupleToList: not enoght elements in list for MonoTuple11"
 
-instance TupleToList (MonoTuple12 a) where
-    tupleFromList (a1:a2:a3::a4:a5:a6:a7:a8:a9:a10:a11:a12:_) = (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12)
+instance (PartTupleList a ~ a) => TupleFromList (MonoTuple12 a) where
+    type PartTupleList (MonoTuple12 a) = a    
+    tupleFromList (a1:a2:a3:a4:a5:a6:a7:a8:a9:a10:a11:a12:_) = (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12)
     tupleFromList _     = error "TupleToList: not enoght elements in list for MonoTuple12"
 
-instance TupleToList (MonoTuple13 a) where
-    tupleFromList (a1:a2:a3::a4:a5:a6:a7:a8:a9:a10:a11:a12:a13:_) = (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13)
+instance (PartTupleList a ~ a) => TupleFromList (MonoTuple13 a) where
+    type PartTupleList (MonoTuple13 a) = a    
+    tupleFromList (a1:a2:a3:a4:a5:a6:a7:a8:a9:a10:a11:a12:a13:_) = (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13)
     tupleFromList _     = error "TupleToList: not enoght elements in list for MonoTuple13"
 
-instance TupleToList (MonoTuple14 a) where
-    tupleFromList (a1:a2:a3::a4:a5:a6:a7:a8:a9:a10:a11:a12:a13:a14:_) = (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14)
+instance (PartTupleList a ~ a) => TupleFromList (MonoTuple14 a) where
+    type PartTupleList (MonoTuple14 a) = a    
+    tupleFromList (a1:a2:a3:a4:a5:a6:a7:a8:a9:a10:a11:a12:a13:a14:_) = (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14)
     tupleFromList _     = error "TupleToList: not enoght elements in list for MonoTuple14"
 
-instance TupleToList (MonoTuple15 a) where
-    tupleFromList (a1:a2:a3::a4:a5:a6:a7:a8:a9:a10:a11:a12:a13:a14:a15:_) = (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15)
+instance (PartTupleList a ~ a) => TupleFromList (MonoTuple15 a) where
+    type PartTupleList (MonoTuple15 a) = a    
+    tupleFromList (a1:a2:a3:a4:a5:a6:a7:a8:a9:a10:a11:a12:a13:a14:a15:_) = (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15)
     tupleFromList _     = error "TupleToList: not enoght elements in list for MonoTuple15"
+
 
 -- | tupleToList converts mono-tuple to list
 --
